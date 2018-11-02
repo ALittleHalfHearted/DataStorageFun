c3c9377479dc56775db3d49173279a6712291527
ALittleHalfHearted <33755588+ALittleHalfHearted@users.noreply.github.com>
2018-11-02 13:57:57 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,12 +3,12 @@ const client = new Discord.Client();
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 	//console.log('Online at\n-----' + D.toString().replace('UTC','CDT').replace('+0000','-0500') + '\n-----' + UTC);
-	client.user.setActivity('type %help for commands');
+	client.user.setActivity('type s~help for commands');
 });
 
 client.on('message', message => {
 	message.content = message.content.toLowerCase()
-	if (message.content.substring(0,1) === '%' && message.author.bot == false) {
+	if (message.content.substring(0,2) === 's~' && message.author.bot == false) {
 		var args = message.content.substring(1).split(' ');
 		var cmd = args[0];
 		args = args.splice(1).toString().replace(/,/g,' ');
