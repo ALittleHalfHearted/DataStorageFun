d9307d087d8fbf5d27d6256daaf908ce86d8e712
ALittleHalfHearted <33755588+ALittleHalfHearted@users.noreply.github.com>
2018-11-02 15:39:45 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client.on('message', message => {
 		args = args.splice(1).toString().replace(/,/g,' ');
     		if(cmd == 'ping'){
     			message.reply('pong');
-			document.createElement("p").innerHTML = message.content;
+			document.getElementById("test").innerHTML = message.content;
 		}
 	}
 });
