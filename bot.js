bc22888ede4fc6403b125c5a832e1bc8e47a52b3
ALittleHalfHearted <33755588+ALittleHalfHearted@users.noreply.github.com>
2018-11-02 14:39:18 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 	//console.log('Online at\n-----' + D.toString().replace('UTC','CDT').replace('+0000','-0500') + '\n-----' + UTC);
@@ -14,7 +15,7 @@ client.on('message', message => {
 		args = args.splice(1).toString().replace(/,/g,' ');
     		if(cmd == 'ping'){
     			message.reply('pong');
-			document.createElement(p).innerHTML = message.content;
+			document.createElement("p").innerHTML = message.content;
 		}
 	}
 });
