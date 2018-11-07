72f3720365a6293b73dbd33dddf243904c70fdd4
ALittleHalfHearted <33755588+ALittleHalfHearted@users.noreply.github.com>
2018-11-07 14:41:06 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,9 +2,9 @@ import os, discord
 from discord.ext import commands
 
 #TOKEN = os.environ['token']
-bot = commands.Bot(command_prefix='s~')
+client = commands.Bot(command_prefix='s~')
 
-@bot.event
+@client.event
 async def on_ready():
     print('Logged in as')
     print(bot.user.name)
@@ -12,8 +12,16 @@ async def on_ready():
     print('------')
     await client.change_presence(game=discord.Game(name="--Insert-Game-name-here--"))
 
-@bot.command()
-async def greet(ctx):
-    await ctx.send(":smiley: :wave: Hello, there!")
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user: # this is to prevent crashing via infinite loops
+        return
+      
+      #conditional branches for commands go below here
+      
+      if message.content.startswith('!hello'): # a simple hello Command
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
 
 bot.run('NTA3OTg3NDIxODc5NTk5MTQ0.DsTNrw.THFXaWPesJkagcT7cxhCEgKiGgU')#TOKEN)
