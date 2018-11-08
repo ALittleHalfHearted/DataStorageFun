62e9a7f16d17b2b46e7fd5a52861c92912a4fe93
ALittleHalfHearted <33755588+ALittleHalfHearted@users.noreply.github.com>
2018-11-07 20:09:35 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,23 @@ async def ping(ctx):
     # Get the latency of the bot
     latency = client.latency  # Included in the Discord.py library
     # Send it to the user
-    await ctx.send(latency)
+    await ctx.send(client.user.name, "\'s latency is: ", latency)
 
+@client.command()
+async def store(ctx, label:str, *, data:str):
+	'''
+	Store data.
+	s~store [data label] [data to store]
+	'''
+	
+	doc = open(ctx.message.server, "a+")
+	
+	docL = doc.readlines()
+	for i in docL:
+		print i
+		#if (i == ctx.message.author):
+		#	x = i
+		#	for x in docL:
+		#		if (
 	
 client.run('NTA3OTg3NDIxODc5NTk5MTQ0.DsTNrw.THFXaWPesJkagcT7cxhCEgKiGgU')#TOKEN)
