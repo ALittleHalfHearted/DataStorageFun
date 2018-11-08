c1af7bad15d57bd4f9124ed7a83a9c5959c62c86
ALittleHalfHearted <33755588+ALittleHalfHearted@users.noreply.github.com>
2018-11-08 13:36:57 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ async def on_ready():
 	print(client.user.name)
 	print(client.user.id)
 	print('------')
-	#await client.change_presence(game=discord.Game(name="s~"))
+	await client.change_presence(game=discord.Game(name="s~"))
 
 @client.command()
 async def ping(ctx):
